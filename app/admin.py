c5fe3393ad91d082cdf6834a522abdb7505324c4
Dyogo Ramos <ramosdyogo75@gmail.com>
2026-03-23 13:46:08 -0300

    Add files via upload

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
-from .models import *
-from django.contrib import admin
+from .models import Cidade, Autor, Editora, Leitor, Livro, Genero
 
+# 1. Definição dos Inlines e Classes Admin
+class LivroInline(admin.TabularInline):
+    model = Livro
+    extra = 1
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+    inlines = [LivroInline]
+
+# 2. Registros (Apenas UM por modelo)
 admin.site.register(Cidade)
-admin.site.register(Autor)
 admin.site.register(Editora)
 admin.site.register(Leitor)
-admin.site.register(Livro)
 admin.site.register(Genero)
+
+# Registrando os modelos que possuem configurações customizadas
+admin.site.register(Livro) 
+admin.site.register(Autor, AutorAdmin)
